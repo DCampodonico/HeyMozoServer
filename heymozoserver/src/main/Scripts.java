f7f7f882b3ef902104c6ba98440057d121015f28
emigioria <emigioria@hotmail.com>
2017-02-10 08:01:34 -0300

    Agregado script para cargar las descripciones largas

--- heymozoserver/src/main/Scripts.java
+++ heymozoserver/src/main/Scripts.java
@@ -1,28 +1,23 @@
 package main;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.Collection;
 
 import org.bson.Document;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 import com.mongodb.MongoClient;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
-import com.mongodb.util.JSON;
 
+@SuppressWarnings("unchecked")
 public class Scripts {
 
 	public static final MongoClient mongoClient = new MongoClient(new ServerAddress("localhost", 27017));
@@ -33,16 +28,43 @@ public class Scripts {
 		setearIdImagenAProductosCarta(1);
 		cargarImagenes();
 		cambiarImagenRestaurante();
-		cambiarImagenRestauranteEnCarta();
+		cambiarImagenRestauranteEnCarta(1);
+		agregarDescripcionesLargas();
 	}
 
-	private static void cambiarImagenRestauranteEnCarta() {
-		int i = 1;
-		Document carta = db.getCollection("cartas").find(Filters.eq("id", 1)).first();
+	private static void agregarDescripcionesLargas() {
+		String path = "./src/datos/descripciones";
+
+		File carpeta = new File(path);
+		File[] descripciones = carpeta.listFiles();
+		for(File archivoDescripcion: descripciones){
+			if(archivoDescripcion.isFile()){
+				String[] nombreDescripcion = archivoDescripcion.getName().split("\\.");
+				String descripcion = fileToString(archivoDescripcion.getAbsolutePath());
+				int idCarta = Integer.valueOf(nombreDescripcion[0]);
+				int idSeccion = Integer.valueOf(nombreDescripcion[1]);
+				int idProducto = Integer.valueOf(nombreDescripcion[2]);
+				Document carta = db.getCollection("cartas").find(Filters.eq("id", idCarta)).first();
+				for(Document seccion: (Collection<Document>) carta.get("secciones")){
+					if(idSeccion == (int) seccion.get("id")){
+						for(Document producto: (Collection<Document>) seccion.get("productos")){
+							if(idProducto == (int) producto.get("id")){
+								producto.put("descripcion_larga", descripcion);
+							}
+						}
+					}
+				}
+				db.getCollection("cartas").replaceOne(Filters.eq("id", idCarta), carta);
+			}
+		}
+	}
+
+	private static void cambiarImagenRestauranteEnCarta(int idCarta) {
+		Document carta = db.getCollection("cartas").find(Filters.eq("id", idCarta)).first();
 		Document restaurante = (Document) carta.get("nombre_restaurant");
 
 		Document imagenDoc = new Document();
-		imagenDoc.append("id", 1);
+		imagenDoc.append("id", idCarta + "");
 		restaurante.append("imagen", imagenDoc);
 
 		db.getCollection("cartas").drop();
@@ -57,11 +79,27 @@ public class Scripts {
 				int idProducto = (int) producto.get("id");
 				Document imagenDoc = new Document();
 				imagenDoc.put("id", idCarta + "." + idSeccion + "." + idProducto);
-				producto.put("imagen", imagenDoc);
+				if(existeImagen(idCarta + "." + idSeccion + "." + idProducto)){
+					producto.put("imagen", imagenDoc);
+				}
 			}
 		}
 		db.getCollection("cartas").drop();
 		db.getCollection("cartas").insertOne(carta);
+	}
+
+	public static Boolean existeImagen(String imagenName) {
+		File folder = new File("./src/datos/imagenes/");
+		File[] listOfFiles = folder.listFiles();
+		for(File file: listOfFiles){
+			if(file.isFile()){
+				String[] filename = file.getName().split("\\.(?=[^\\.]+$)");
+				if(filename[0].equalsIgnoreCase(imagenName)){
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 
 	public static void cambiarImagenRestaurante() {
@@ -70,7 +108,7 @@ public class Scripts {
 			restaurantes.add(restaurante);
 
 			Document imagenDoc = new Document();
-			imagenDoc.append("id", Double.valueOf(restaurante.get("id") + "").intValue());
+			imagenDoc.append("id", restaurante.get("id") + "");
 			restaurante.append("imagen", imagenDoc);
 			restaurante.remove("imagen64");
 		});
@@ -79,64 +117,55 @@ public class Scripts {
 	}
 
 	private static void cargarImagenes() {
-		db.getCollection("imagenes").drop();	
+		db.getCollection("imagenes").drop();
 		String path = "./src/datos/imagenes";
 
-        String nombreImagen;
-        File carpeta = new File(path);
-        File[] imagenes = carpeta.listFiles();
+		String nombreImagen;
+		File carpeta = new File(path);
+		File[] imagenes = carpeta.listFiles();
 
-        for (File i: imagenes){
+		for(File i: imagenes){
 
-            if (i.isFile()){
-            	nombreImagen = i.getName().substring(0, i.getName().lastIndexOf('.'));
+			if(i.isFile()){
+				nombreImagen = i.getName().substring(0, i.getName().lastIndexOf('.'));
 				FileInputStream imagenInputStream;
-				try {
+				try{
 					imagenInputStream = new FileInputStream(i);
-					
-		            byte imagenByteArray[] = new byte[(int) i.length()];
-		            imagenInputStream.read(imagenByteArray);
-		 
+
+					byte imagenByteArray[] = new byte[(int) i.length()];
+					imagenInputStream.read(imagenByteArray);
+
 					String imagenBase64 = Base64.getEncoder().encodeToString(imagenByteArray);
-					
+
 					Document imagen = new Document();
 					imagen.put("id", nombreImagen);
 					imagen.put("imagen", imagenBase64);
 					db.getCollection("imagenes").insertOne(imagen);
-					
-				} catch (IOException e) {
+
+				} catch(IOException e){
 					e.printStackTrace();
 				}
-            }
-        }
+			}
+		}
 	}
-	
-	private static void fileToDB(String filePath, String collection){
-		
-		JSONParser parser = new JSONParser();
-		JSONObject a;
-		try {
-			a = (JSONObject) parser.parse(new FileReader(filePath));
-			
-			//for (Object o : a){
-				db.getCollection(collection).insertOne(Document.parse(a.toString()));
-				//}
-		} catch (ParseException e) {
-			e.printStackTrace();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
+
+	private static String fileToString(String filePath) {
+		try{
+			byte[] encoded = Files.readAllBytes(Paths.get(filePath));
+			return new String(encoded, StandardCharsets.UTF_8);
+		} catch(IOException e){
 			e.printStackTrace();
 		}
+		return null;
 	}
 
 	private static void init() {
 		db.getCollection("restaurantes").drop();
-		fileToDB(new File("src/datos/restaurantes.json").getAbsolutePath(),"restaurantes");
-		
+		db.getCollection("restaurantes").insertOne(Document.parse(fileToString(new File("src/datos/restaurantes.json").getAbsolutePath())));
+
 		db.getCollection("cartas").drop();
-		fileToDB(new File("src/datos/cartas.json").getAbsolutePath(),"cartas");
-		
+		db.getCollection("cartas").insertOne(Document.parse(fileToString(new File("src/datos/cartas.json").getAbsolutePath())));
+
 		db.getCollection("pedidos").drop();
 	}
 }
